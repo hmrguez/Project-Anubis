2459e0aa6170158dacf2c437235998701f3dcb61
vekt0R-HUB <zealot.algo@gmail.com>
2023-06-25 12:20:44 -0400

    Initial commit

---
+++ src/main/java/com/example/anubis/AnubisApplication.java
@@ -0,0 +1,13 @@
+package com.example.anubis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnubisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnubisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anubis/AnubisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anubis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnubisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
