b5dc81f88636ddf62fb66cd94d6a725e71d59892
vekt0R-HUB <zealot.algo@gmail.com>
2023-06-25 15:58:39 -0400

    Added Kafka config

--- src/main/java/com/anubis/config/KafkaTopicConfig.java
+++ src/main/java/com/anubis/config/KafkaTopicConfig.java
@@ -1,5 +1,15 @@
 package com.anubis.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
 
+    @Bean
+    public NewTopic petTopic(){
+        return TopicBuilder.name("pet").build();
+    }
 }
